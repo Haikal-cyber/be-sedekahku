fd28dd547c60a6ad5133bc165f85d1d4951840c5
Haikal-cyber <fikri.haikal234@gmail.com>
2025-07-21 10:58:48 +0700

    feat: add name in jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ module.exports = {
       if (!user) return res.status(404).json({ message: 'User tidak ditemukan' });
       const valid = await bcrypt.compare(password, user.password);
       if (!valid) return res.status(401).json({ message: 'Password salah' });
-      const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+      const token = jwt.sign({ id: user.id, name: user.name, email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
       res.json({ message: 'Login berhasil', token });
     } catch (err) {
       res.status(500).json({ message: 'Server error', error: err.message });
